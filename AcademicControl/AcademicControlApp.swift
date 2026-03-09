9b19b6460b026c21d3c2e7d5d0debb2ab2b721f3
UltiimateDog <alan.perezhz@icloud.com>
2026-03-09 08:33:24 -0600

    Initial Commit

---
+++ AcademicControl/AcademicControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademicControlApp.swift
+//  AcademicControl
+//
+//  Created by Ultiimate Dog on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AcademicControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AcademicControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AcademicControl
+//
+//  Created by Ultiimate Dog on 09/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
